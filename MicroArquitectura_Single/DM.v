651c991c57d2073e325d5373b878df4ed4625610
VaneSarahi <A01646141@tec.mx>
2026-05-31 16:51:41 -0600

    Update DM.v

--- MicroArquitectura_Single/DM.v
+++ MicroArquitectura_Single/DM.v
@@ -1,6 +1,19 @@
 module DM(
-
+    input clk, 
+    input [31:0] A, 
+    input WE, 
+    input [31:0] WD, 
+    output [31:0] ReadData
 );
 
+reg [31:0] data_mem [0:255];
+
+assign ReadData= data_mem[A[31:2]];
+
+always @(posedge clk)
+    begin 
+        if (WE)
+        data_mem[A[31:2]] <=WD;
+    end 
 
 endmodule 
